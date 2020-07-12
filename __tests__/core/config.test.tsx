c0f0794675babaa555886b345fa5805f58d3273e
Thinh Tran <quangthinhtran3588@gmail.com>
2020-07-12 17:06:20 +0700

    update config test

---
+++ __tests__/core/config.test.tsx
@@ -0,0 +1,75 @@
+import {config, updateConfig} from '@app/core/config';
+
+it('update configuration correctly', async () => {
+  expect(config()).toMatchInlineSnapshot(`
+    Object {
+      "android": Object {
+        "id": "pro.thinhtran.starterkit",
+      },
+      "appInfo": Object {
+        "build": "1",
+        "name": "TQT Starter Kit",
+        "version": "0.1",
+      },
+      "dateFormat": "DD/MM/YYYY",
+      "defaultLang": "vi",
+      "fb": Object {
+        "id": "",
+      },
+      "ios": Object {
+        "id": "pro.thinhtran.starterkit",
+      },
+      "languages": Array [
+        Object {
+          "id": "en",
+          "name": "English",
+        },
+        Object {
+          "id": "vi",
+          "name": "Tiếng Việt",
+        },
+      ],
+    }
+  `);
+  updateConfig({
+    android: {
+      id: 'pro.thinhtran.test',
+    },
+    appInfo: {
+      name: 'TQT Starter Kit Test',
+    },
+    ios: {
+      id: 'pro.thinhtran.test',
+    },
+  });
+  expect(config()).toMatchInlineSnapshot(`
+    Object {
+      "android": Object {
+        "id": "pro.thinhtran.test",
+      },
+      "appInfo": Object {
+        "build": "1",
+        "name": "TQT Starter Kit Test",
+        "version": "0.1",
+      },
+      "dateFormat": "DD/MM/YYYY",
+      "defaultLang": "vi",
+      "fb": Object {
+        "id": "",
+      },
+      "ios": Object {
+        "id": "pro.thinhtran.test",
+      },
+      "languages": Array [
+        Object {
+          "id": "en",
+          "name": "English",
+        },
+        Object {
+          "id": "vi",
+          "name": "Tiếng Việt",
+        },
+      ],
+    }
+  `);
+});
