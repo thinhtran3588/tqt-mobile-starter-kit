454d05cefd3c1dd37bae7a180d4368bc44e462f1
Thinh Tran <quangthinhtran3588@gmail.com>
2020-07-27 09:04:18 +0700

    remove unused tests

--- __tests__/core/contexts/app-theme.context.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render} from '@test-utils';
-import {SettingsScreen} from '@settings/screens';
-
-it('throws error if not wrapped by AppThemeProvider', async () => {
-  // expect(() => render(<SettingsScreen />, undefined, false)).toThrowError();
-});
