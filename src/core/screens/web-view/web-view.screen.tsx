af6e85771157e1a97890c7584bcb72b96721fe09
Thinh Tran <quangthinhtran3588@gmail.com>
2020-07-30 21:57:59 +0700

    restrict webview to open only whitelisted urls

--- src/core/screens/web-view/web-view.screen.tsx
+++ src/core/screens/web-view/web-view.screen.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {createRef} from 'react';
+import {WebViewNavigation} from 'react-native-webview';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {Layout, WebView} from '@core/components';
 
@@ -7,17 +8,31 @@ interface WebViewParams {
   url: string;
 }
 
+const WHITELIST_URLS = ['https://thinhtran.pro'];
+
 export const WebViewScreen = (): JSX.Element => {
   const navigation = useNavigation();
   const route = useRoute();
   const {title, url}: WebViewParams = route.params as WebViewParams;
+  const webviewRef = createRef<WebView>();
+
   const goBack = (): void => {
     navigation.goBack();
   };
 
+  const handleWebViewNavigationStateChange = (event: WebViewNavigation): void => {
+    const {url: newUrl} = event;
+    if (!url) return;
+
+    // only whitelisted websites can be processed
+    if (!WHITELIST_URLS.find((whitelistUrl) => newUrl.indexOf(whitelistUrl) > -1) && webviewRef.current) {
+      webviewRef.current.stopLoading();
+    }
+  };
+
   return (
     <Layout header headerTitle={title} headerBackButton headerBackButtonOnPress={goBack}>
-      <WebView source={{uri: url}} />
+      <WebView ref={webviewRef} source={{uri: url}} onNavigationStateChange={handleWebViewNavigationStateChange} />
     </Layout>
   );
 };
