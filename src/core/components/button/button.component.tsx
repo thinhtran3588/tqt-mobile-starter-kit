5f21ebfdf0cd9ccdfa2ec08c605156e68a9e98d6
Thinh Tran <quangthinhtran3588@gmail.com>
2020-08-01 21:40:05 +0700

    update button style

--- src/core/components/button/button.component.tsx
+++ src/core/components/button/button.component.tsx
@@ -1,3 +1,10 @@
-import {Button} from 'react-native-paper';
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import {Button as RNButton} from 'react-native-paper';
+import {styles} from './button.styles';
 
+const Button = (props: React.ComponentProps<typeof RNButton>): JSX.Element => {
+  const {style, uppercase = false, ...other} = props;
+  return <RNButton style={[styles.button, style]} uppercase={uppercase} {...other} />;
+};
 export {Button};
