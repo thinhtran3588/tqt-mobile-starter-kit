183b253ed9057bf9c8aab03dce6365c534874056
Thinh Tran <quangthinhtran3588@gmail.com>
2020-08-12 13:11:08 +0700

    update to call Sentry only in release mode

--- src/core/exceptions/record-error.ts
+++ src/core/exceptions/record-error.ts
@@ -1,4 +1,11 @@
 import * as Sentry from '@sentry/react-native';
+import {config} from '@core/config';
+
+if (!__DEV__) {
+  Sentry.init({
+    dsn: config().sentry.dns,
+  });
+}
 
 export interface ConfigErrorParams {
   userId: string;
@@ -6,14 +13,16 @@ export interface ConfigErrorParams {
 
 export const configError = (params: ConfigErrorParams): void => {
   const {userId} = params;
-  Sentry.setUser({id: userId});
+  if (!__DEV__) {
+    Sentry.setUser({id: userId});
+  }
 };
 
 export const recordError = async (err: Error): Promise<void> => {
-  if (__DEV__) {
+  if (!__DEV__) {
+    Sentry.captureException(err);
+  } else {
     // eslint-disable-next-line no-console
     console.log('record err', err);
-    return;
   }
-  Sentry.captureException(err);
 };
