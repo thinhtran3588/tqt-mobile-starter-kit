02dd7afc826b9d900859c977f441106680290b06
Thinh Tran <quangthinhtran3588@gmail.com>
2020-08-12 17:41:55 +0700

    mock date in tests

--- __tests__/modules/samples/screens/component-list/component-list.screen.test.tsx
+++ __tests__/modules/samples/screens/component-list/component-list.screen.test.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import {Platform, Alert} from 'react-native';
+import MockDate from 'mockdate';
 import {render, toJSON, fireEvent} from '@test-utils';
 import {ComponentListScreen} from '@samples/screens/component-list/component-list.screen';
 
-beforeAll(() => {
+beforeEach(() => {
   // mock alert
   Alert.alert = jest.fn();
+  MockDate.set(new Date(2020, 0, 1));
 });
 
 afterEach(() => {
   jest.resetAllMocks();
+  MockDate.reset();
 });
 
 it('renders correctly', async () => {
   const {baseElement} = render(<ComponentListScreen />);
+  // I use a timestamp to make sure the date stays fixed to the ms
+  // test code here
+  // reset to native Date()
   expect(toJSON(baseElement)).toMatchSnapshot();
 });
 
